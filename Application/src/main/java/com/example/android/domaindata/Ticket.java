5473f8254f9bd115e8e59ba8e2e03bbfc042e36f
Estelle Rostan <estelle.rostan@etu.unice.fr>
2018-07-31 20:04:49 +0200

    Add ticket not added in #a0488c6 by mistake

---
+++ Application/src/main/java/com/example/android/domaindata/Ticket.java
@@ -0,0 +1,29 @@
+package com.example.android.domaindata;
+
+public class Ticket {
+    private String name;
+    private String description;
+
+    public Ticket() {}
+
+    public Ticket(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
